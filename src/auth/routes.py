0a7c06dff4c1bcbd30c1ee0d9ab89d85edebe6a3
msalahuddin267 <msalahuddin267@gmail.com>
2024-12-03 01:15:47 +0600

    auth JWT

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,13 +1,18 @@
 from fastapi import APIRouter, Depends, status
 from fastapi.exceptions import HTTPException
 from sqlmodel.ext.asyncio.session import AsyncSession
-from .schemas import UserCreateModel, UserModel
+from .schemas import UserCreateModel, UserModel, UserLoginModel
 from .service import UserService
 from src.db.main import get_session
+from .utils import create_access_token, decode_token, verify_password
+from datetime import timedelta
+from fastapi.responses import JSONResponse
 
 
 auth_router = APIRouter()
 user_service = UserService()
+
+REFRESH_TOKEN_EXPIRY = 2
 
 @auth_router.post("/signup", response_model=UserModel, status_code=status.HTTP_201_CREATED)
 async def create_user_Account(user_data: UserCreateModel, session: AsyncSession = Depends(get_session)):
@@ -21,3 +26,47 @@ async def create_user_Account(user_data: UserCreateModel, session: AsyncSession 
     new_user = await user_service.create_user(user_data, session)
     
     return new_user
+
+@auth_router.post("/login", response_model=UserLoginModel)
+async def login_users(login_data: UserLoginModel, session: AsyncSession = Depends(get_session)):
+    email = login_data.email
+    password = login_data.password
+    
+    user = await user_service.get_user_by_email(email, session)
+    
+    if user is not None:
+        password_valid = verify_password(password, user.password_hash)
+        
+        if password_valid:
+            access_token = create_access_token(
+                user_data={
+                    "email": user.email,
+                    "user_uid": str(user.uid)
+                }
+            )
+            
+            refresh_token = create_access_token(
+                user_data={
+                    "email": user.email,
+                    "user_uid": str(user.uid)
+                }
+                refresh=True,
+                expiry=timedelta(days=REFRESH_TOKEN_EXPIRY)
+            )
+            
+            return JSONResponse(
+                content={
+                    "message": "Login Successful",
+                    "access_token": access_token,
+                    "refresh_token": refresh_token,
+                    "user":{
+                        "email": user.email,
+                        "user_uid": str(user.uid)
+                    }
+                }
+            )
+            
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="Invalid Email or Password"
+    )
