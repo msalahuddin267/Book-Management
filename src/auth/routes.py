86e33a85feeea3e3a49dcaf1ba780ac1b3e1b20e
msalahuddin267 <msalahuddin267@gmail.com>
2024-12-05 22:22:17 +0600

    new access token

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -5,7 +5,8 @@ from .schemas import UserCreateModel, UserModel, UserLoginModel
 from .service import UserService
 from src.db.main import get_session
 from .utils import create_access_token, decode_token, verify_password
-from datetime import timedelta
+from .dependencies import RefreshTokenBearer
+from datetime import timedelta, datetime
 from fastapi.responses import JSONResponse
 
 
@@ -69,4 +70,18 @@ async def login_users(login_data: UserLoginModel, session: AsyncSession = Depend
     raise HTTPException(
         status_code=status.HTTP_403_FORBIDDEN,
         detail="Invalid Email or Password"
+    )
+    
+@auth_router.get("/refresh_token")
+async def get_new_access_token(token_details: dict = Depends(RefreshTokenBearer())):
+    expiry_timestamp = token_details["exp"]
+    
+    if datetime.fromtimestamp(expiry_timestamp) > datetime.now():
+        new_access_token = create_access_token(user_data=token_details["user"])
+        
+        return JSONResponse(content={"access_token" : new_access_token})
+    
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="Invalid or expired token"
     )
