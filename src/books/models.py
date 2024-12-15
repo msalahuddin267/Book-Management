b73f87aca614cfaef1504615b23d211c1ba2eaae
msalahuddin267 <msalahuddin267@gmail.com>
2024-12-16 00:23:52 +0600

    add schema and create books table

--- src/books/models.py
+++ src/books/models.py
@@ -1,13 +1,27 @@
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel, Field, Column
 from datetime import datetime
+import sqlalchemy.dialects.postgresql as pg
+import uuid
 
 class Book(SQLModel, table=True):
-    id: int
+    __tablename__ = "books"
+    
+    uid: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            nullable=False,
+            primary_key=True,
+            default=uuid.uuid4()
+        )
+    )
     title: str
     author: str
     publisher: str
     publish_date: str
     page_count: int
     language: str
-    created_at: datetime
-    updated_at: datetime
+    created_at: datetime = Field(Column(pg.TIMESTAMP, default=datetime.now))
+    updated_at: datetime = Field(Column(pg.TIMESTAMP, default=datetime.now))
+    
+    def __repr__(self):
+        return f"<Book {self.title}>"
--- src/db/main.py
+++ src/db/main.py
@@ -1,4 +1,4 @@
-from sqlmodel import create_engine, text
+from sqlmodel import create_engine, text, SQLModel
 from sqlalchemy.ext.asyncio import AsyncEngine
 from src.config import Config
 
@@ -11,6 +11,6 @@ engine = AsyncEngine(
 
 async def init_db():
     async with engine.begin() as conn:
-        statement = text("SELECT 'hello';")
-        result = await conn.execute(statement)
-        print(result.all())
+        from src.books.models import Book
+        
+        await conn.run_sync(SQLModel.metadata.create_all)
